b2068433bdc715ab22e6d80fbcc0fd4d72140a32
Cengizhan Atakul <cengizatakul@hotmail.de>
2022-08-05 16:06:21 +0200

    add and fill react modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "./App.css";
-import PeriodicTable from "./Components/PeriodicTable.js";
+import PeriodicTable from './Components/PeriodicTable.js';
+import styled from 'styled-components';
 
 export default function App() {
   return (
-    <div className="App">
-<h1>Periodic Table of Elements</h1>
-<PeriodicTable />
-    </div>
+    <StyledBody>
+      <header>
+        <h1>Periodic Table of Elements</h1>
+      </header>
+      <main>
+        <PeriodicTable />
+      </main>
+
+      {/* <nav>
+          <p>navigation test</p>
+          <p>navigation test</p>
+          <p>navigation test</p>
+        </nav> */}
+    </StyledBody>
   );
 }
+
+const StyledBody = styled.div`
+  overflow-y: scroll;
+  overflow-x: scroll;
+  scrollbar-width: none;
+`;
+
+const MyHeader = styled.header`
+  h1 {
+    color: red;
+  }
+`;
+
+// const MyFooter = styled.footer`
+//   nav {
+//     color: red;
+//   }
+// `;
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -10,13 +10,22 @@ export default styled.createGlobalStyle`
 
   body {
     margin: 0;
+    padding: 0;
     font-size: 1.125rem;
     font-family: sans-serif;
+    text-align: center;
+  }
+
+  main {
+    padding: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   button,
   input,
   textarea {
     font: inherit;
-  } 
+  }
 `;
